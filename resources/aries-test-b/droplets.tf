64981b929295b833bb1f469d88320e18deaffb89
1devndogs <leo@subspace.network>
2021-12-13 18:51:01 -0300

    Remove farmers volumes, use s-4vcpu-8gb-intel

--- resources/aries-test-b/droplets.tf
+++ resources/aries-test-b/droplets.tf
@@ -14,7 +14,7 @@ resource "digitalocean_droplet" "aries-farmers-network-b-nodes" {
   image  = "ubuntu-20-04-x64"
   name   = "aries-farmers-network-b-nodes"
   region = "nyc3"
-  size   = "c-8"
+  size   = "s-4vcpu-8gb-intel"
   ssh_keys = [
     data.digitalocean_ssh_key.nazar-key.id,
     data.digitalocean_ssh_key.serge-key.id,
