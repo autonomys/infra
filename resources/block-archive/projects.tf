409b45e4308a9ce43594ecc337ac194c4cc28671
1devndogs <leo@subspace.network>
2021-12-13 13:58:31 -0300

    Remove old sfo block archive resources

--- resources/block-archive/projects.tf
+++
@@ -1,8 +0,0 @@
-resource "digitalocean_project" "block-archive" {
-  name        = "block-archive"
-  description = "Block Archive"
-  purpose     = "Service or API"
-  environment = "development"
-  resources   = [digitalocean_droplet.subspace-nodes-relayer-backend-2021.urn]
-}
-
