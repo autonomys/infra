50e1da0b7c40d97253223b84d2f9216953b14b87
1devndogs <leo@subspace.network>
2022-01-31 23:12:37 -0300

    Update dev volume name

--- resources/aries-dev/volumes.tf
+++ resources/aries-dev/volumes.tf
@@ -1,13 +1,13 @@
 # DEVELOPMENT
-resource "digitalocean_volume" "aries-dev-relayer-volume" {
+resource "digitalocean_volume" "aries-dev-volume" {
   region                  = "nyc3"
-  name                    = "aries-dev-relayer-volume"
+  name                    = "aries-dev-volume"
   size                    = 1100
   initial_filesystem_type = "ext4"
-  description             = "Extra volume for relayer data initial imports."
+  description             = "Extra volume for aries dev."
 }
 
-resource "digitalocean_volume_attachment" "aries-dev-relayer-volume" {
+resource "digitalocean_volume_attachment" "aries-dev-volume" {
   droplet_id = digitalocean_droplet.aries-dev-nodes-farmer-relayer.id
-  volume_id  = digitalocean_volume.aries-dev-relayer-volume.id
+  volume_id  = digitalocean_volume.aries-dev-volume.id
 }
