f0c3facaf02265bbd4786c44d61a6269077f3883
1devndogs <vsalgado.a@gmail.com>
2022-04-05 02:04:49 -0400

    Resources for Subspace Telemetry

---
+++ resources/telemetry/provider.tf
@@ -0,0 +1,9 @@
+provider "digitalocean" {
+  token = var.do_token
+}
+data "digitalocean_ssh_key" "nazar-key" {
+  name = "Nazar SSH Key"
+}
+data "digitalocean_ssh_key" "leo-key" {
+  name = "Leo SSH Key"
+}
