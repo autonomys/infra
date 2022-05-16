97987504c47ca625ca6c52b57ae3b7bf689cb0a8
Genysys <samuel.b.dare@gmail.com>
2022-05-16 15:44:49 +0400

    chore: correct ip output name

--- resources/farmnet-a/outputs.tf
+++ resources/farmnet-a/outputs.tf
@@ -1,4 +1,4 @@
-output "ip_address_test_droplet" {
+output "ip_address_aries-farmnet-a_droplet" {
   value       = digitalocean_droplet.aries-farmnet-a.ipv4_address
   description = "ip_address_droplet: "
 }
