40a4f7f5e2e479b3c66ded8d1727c1016dcadd7d
vedhavyas <vedhavyas.singareddi@gmail.com>
2023-03-28 18:51:33 +0200

    add gemini-3d resource

---
+++ resources/gemini-3d/main.tf
@@ -0,0 +1,59 @@
+module "gemini-3d" {
+  source          = "../../network-primitives"
+  path-to-scripts = "../../network-primitives/scripts"
+  network-name    = "gemini-3d"
+  bootstrap-node-config = {
+    droplet_size        = var.droplet-size
+    deployment-version  = 1
+    regions             = []
+    nodes-per-region    = 0
+    additional-node-ips = var.hetzner_bootstrap_node_ips
+    docker-org          = "subspace"
+    docker-tag          = "snapshot-2023-mar-28"
+    reserved-only       = false
+    prune               = true
+  }
+  full-node-config = {
+    droplet_size        = var.droplet-size
+    deployment-version  = 1
+    regions             = []
+    nodes-per-region    = 0
+    additional-node-ips = var.hetzner_full_node_ips
+    docker-org          = "subspace"
+    docker-tag          = "snapshot-2023-mar-28"
+    reserved-only       = false
+    prune               = true
+  }
+  rpc-node-config = {
+    droplet_size        = var.droplet-size
+    deployment-version  = 1
+    regions             = []
+    nodes-per-region    = 0
+    additional-node-ips = var.hetzner_rpc_node_ips
+    docker-org          = "subspace"
+    docker-tag          = "snapshot-2023-mar-28"
+    domain-prefix       = "eu"
+    reserved-only       = false
+    prune               = true
+  }
+  farmer-node-config = {
+    droplet_size           = var.droplet-size
+    deployment-version     = 1
+    regions                = []
+    nodes-per-region       = 0
+    additional-node-ips    = []
+    docker-org             = "subspace"
+    docker-tag             = "snapshot-2023-mar-28"
+    reserved-only          = false
+    plot-size              = "10G"
+    reward-address         = var.farmer-reward-address
+    force-block-production = false
+    prune                  = false
+  }
+  piece_cache_size     = "50G"
+  cloudflare_api_token = var.cloudflare_api_token
+  cloudflare_email     = var.cloudflare_email
+  do_token             = var.do_token
+  ssh_identity         = var.ssh_identity
+  datadog_api_key      = var.datadog_api_key
+}
