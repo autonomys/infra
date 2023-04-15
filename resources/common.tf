3e7fae4d72b7f2238f8ee457f8184569509394d0
DaMandal0rian <mando.github@proton.me>
2023-04-15 11:27:46 +0000

    - add kubernetes templates for explorer components - add user ssh to terraform

--- resources/common.tf
+++ resources/common.tf
@@ -54,11 +54,16 @@ data "digitalocean_ssh_key" "i1i1-key" {
   name = "Ivan SSH Key"
 }
 
+data "digitalocean_ssh_key" "muhammad-key" {
+  name = "Muhammad SSH Key"
+}
+
 # add ssh keys as single var
 locals {
   ssh_keys = [
     data.digitalocean_ssh_key.nazar-key.id,
     data.digitalocean_ssh_key.serge-key.id,
-    data.digitalocean_ssh_key.ved-key.id
+    data.digitalocean_ssh_key.ved-key.id,
+    data.digitalocean_ssh_key.muhammad-key.id
   ]
 }
