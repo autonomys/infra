059da0aa8a2c486a6dc5eb97fa62646aea06ea4e
DaMandal0rian <mando.github@proton.me>
2023-05-16 14:51:50 +0000

    remove k8s runners

--- github-runners/k8s/helm/helm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#export GITHUB_TOKEN=$1
-echo $GITHUB_TOKEN
-kubectl create secret generic controller-manager  -n actions-runner-system --from-literal=github_token=${GITHUB_TOKEN}
-helm upgrade --install --namespace actions-runner-system  --create-namespace --wait actions-runner-controller actions-runner-controller/actions-runner-controller --set syncPeriod=1m
