6be0f8bdaca42204af453a755239ff2c216fbe42
DaMandal0rian <mando.github@proton.me>
2023-06-01 21:06:12 +0000

    Fix http/https ingress cidr

--- aws/telemetry/ec2/base/network.tf
+++ aws/telemetry/ec2/base/network.tf
@@ -76,7 +76,7 @@ resource "aws_security_group" "telemetry-subspace-sg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.telemetry-vpc.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -84,7 +84,7 @@ resource "aws_security_group" "telemetry-subspace-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.telemetry-vpc.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
