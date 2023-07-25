7c60b27f74e47cce0f0d75b87c7c6a8db716cc35
DaMandal0rian <mando.github@proton.me>
2023-07-25 16:31:42 +0300

    add subspace.net to terraform

--- resources/dns/outputs.tf
+++ resources/dns/outputs.tf
@@ -2,3 +2,8 @@ output "cloudflare-zone" {
   value       = data.cloudflare_zone.cloudflare_zone.id
   description = "Cloudflare's subspace.network Zone ID"
 }
+
+output "cloudflare-subspace-net-zone" {
+  value       = data.cloudflare_zone.cloudflare_zone_subspace_net.id
+  description = "Cloudflare's subspace.net Zone ID"
+}
