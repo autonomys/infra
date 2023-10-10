7fe9f8a1c17d75836ee6c96bcdcb0444e9f20d04
DaMandal0rian <mando.github@proton.me>
2023-10-10 17:47:09 +0300

    remove defaults

--- cloudflare-lb/cloudflare_lb_module/variables.tf
+++ cloudflare-lb/cloudflare_lb_module/variables.tf
@@ -36,22 +36,6 @@ variable "records" {
     type     = string
     tags     = list(string)
   }))
-  default = [
-    {
-      name     = "rpc-0"
-      hostname = "rpc.gemini-3f.subspace.network"
-      value    = "52.91.27.239"
-      type     = "A"
-      tags     = ["rpc", "us"]
-    },
-    {
-      name     = "rpc-1"
-      hostname = "rpc.gemini-3f.subspace.network"
-      value    = "65.108.232.52"
-      type     = "A"
-      tags     = ["rpc", "eu"]
-    },
-  ]
 }
 
 variable "evm_records" {
@@ -63,22 +47,6 @@ variable "evm_records" {
     type     = string
     tags     = list(string)
   }))
-  default = [
-    {
-      name     = "evm-0"
-      hostname = "domain-3.gemini-3f.subspace.network"
-      value    = "174.129.202.104"
-      type     = "A"
-      tags     = ["domain", "evm", "us"]
-    },
-    {
-      name     = "evm-1"
-      hostname = "domain-3.gemini-3f.subspace.network"
-      value    = "65.108.228.84"
-      type     = "A"
-      tags     = ["domain", "evm", "eu"]
-    },
-  ]
 }
 
 variable "monitors" {
@@ -92,26 +60,6 @@ variable "monitors" {
     port : number
     description : string
   }))
-  default = [
-    {
-      name           = "check-rpc-tcp"
-      expected_codes = "2xx"
-      timeout        = 5
-      interval       = 60
-      retries        = 2
-      port           = 30333
-      description    = "TCP Check port- expect 2xx"
-    },
-    {
-      name           = "check-evm-tcp"
-      expected_codes = "2xx"
-      timeout        = 5
-      interval       = 60
-      retries        = 2
-      port           = 30303
-      description    = "EVM Check port- expect 2xx"
-    }
-  ]
 }
 
 variable "load_balancers" {
