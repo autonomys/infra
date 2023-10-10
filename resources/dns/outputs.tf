ebe6f9ff807df7d1410305bbe410a0aa1167017d
DaMandal0rian <3614052+DaMandal0rian@users.noreply.github.com>
2023-10-10 15:13:48 +0100

    DNS records changes (#185)
    - add continuim.cc
    - add subdomains for subspace.network
    - renaming resources to follow convention
    - minor change resource name

--- resources/dns/outputs.tf
+++ resources/dns/outputs.tf
@@ -1,9 +1,14 @@
 output "cloudflare-zone" {
-  value       = data.cloudflare_zone.cloudflare_zone.id
+  value       = data.cloudflare_zone.subspace_network.id
   description = "Cloudflare's subspace.network Zone ID"
 }
 
 output "cloudflare-subspace-net-zone" {
-  value       = data.cloudflare_zone.cloudflare_zone_subspace_net.id
+  value       = data.cloudflare_zone.subspace_net.id
   description = "Cloudflare's subspace.net Zone ID"
+}
+
+output "cloudflare-continuim-cc-zone" {
+  value       = data.cloudflare_zone.continuim_cc.id
+  description = "Cloudflare's continuim.cc Zone ID"
 }
--- resources/dns/records.tf
+++ resources/dns/records.tf
@@ -1,7 +1,3 @@
-data "cloudflare_zone" "cloudflare_zone" {
-  name = "subspace.network"
-}
-
 // subspace blog
 resource "cloudflare_record" "blog_0" {
   comment = "Medium .blog Redirect #2"
@@ -10,7 +6,7 @@ resource "cloudflare_record" "blog_0" {
   ttl     = 3600
   type    = "A"
   value   = "162.159.152.4"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "blog_1" {
@@ -20,7 +16,7 @@ resource "cloudflare_record" "blog_1" {
   ttl     = 3600
   type    = "A"
   value   = "162.159.153.4"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 // subspace docs
@@ -31,7 +27,7 @@ resource "cloudflare_record" "docs" {
   ttl     = 1
   type    = "CNAME"
   value   = "subspace.github.io"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 // subspace forum
@@ -42,7 +38,7 @@ resource "cloudflare_record" "forum" {
   ttl     = 60
   type    = "CNAME"
   value   = "subspace.hosted-by-discourse.com"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 // subspace status page
@@ -53,7 +49,7 @@ resource "cloudflare_record" "status" {
   ttl     = 3600
   type    = "CNAME"
   value   = "subspace.github.io"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 // subspace subnomicon
@@ -64,7 +60,7 @@ resource "cloudflare_record" "subnomicon" {
   ttl     = 1
   type    = "CNAME"
   value   = "subspace.github.io"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 // subspace uptime
@@ -75,7 +71,40 @@ resource "cloudflare_record" "uptime" {
   ttl     = 1
   type    = "A"
   value   = "35.89.37.220"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
+}
+
+// subspace Grafana monitor
+resource "cloudflare_record" "grafana" {
+  comment = "Grafana and Loki monitoring"
+  name    = "grafana"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = "138.201.195.122"
+  zone_id = data.cloudflare_zone.subspace_network.id
+}
+
+// subspace Loki logs
+resource "cloudflare_record" "logging" {
+  comment = "Loki logging server"
+  name    = "grafana"
+  proxied = false
+  ttl     = 3600
+  type    = "A"
+  value   = "138.201.195.122"
+  zone_id = data.cloudflare_zone.subspace_network.id
+}
+
+// subspace victoria metrics monitoring (temporary)
+resource "cloudflare_record" "vmetrics" {
+  comment = "Victoria Metrics monitoring"
+  name    = "vmetrics"
+  proxied = false
+  ttl     = 3600
+  type    = "A"
+  value   = "138.201.195.122"
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 // subspace block explorer
@@ -86,7 +115,7 @@ resource "cloudflare_record" "block_explorer" {
   ttl     = 3600
   type    = "CNAME"
   value   = "subspace.github.io"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 // subspace blockscout
@@ -97,7 +126,7 @@ resource "cloudflare_record" "blockscout" {
   ttl     = 1
   type    = "A"
   value   = "3.145.138.108"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 
@@ -108,7 +137,7 @@ resource "cloudflare_record" "terraform_managed_resource_964bf73d0853cefc64803ba
   ttl     = 1
   type    = "A"
   value   = "65.108.232.54"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_e59b1773ac2b8ecbff7999562a42e8f9" {
@@ -117,7 +146,7 @@ resource "cloudflare_record" "terraform_managed_resource_e59b1773ac2b8ecbff79995
   ttl     = 3600
   type    = "CNAME"
   value   = "u27463899.wl150.sendgrid.net"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_55771960964e20edcd05f5319f7b7b63" {
@@ -126,7 +155,7 @@ resource "cloudflare_record" "terraform_managed_resource_55771960964e20edcd05f53
   ttl     = 3600
   type    = "CNAME"
   value   = "aries-farm-rpc-b.subspace.network"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_8c34fb58ccc82ccee8a5aa916c6aee55" {
@@ -135,7 +164,7 @@ resource "cloudflare_record" "terraform_managed_resource_8c34fb58ccc82ccee8a5aa9
   ttl     = 60
   type    = "CNAME"
   value   = "dkim2.mcsv.net"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_8c342db13e3ca5d4c407a5cc8a056f84" {
@@ -144,7 +173,7 @@ resource "cloudflare_record" "terraform_managed_resource_8c342db13e3ca5d4c407a5c
   ttl     = 60
   type    = "CNAME"
   value   = "dkim3.mcsv.net"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_c6c12f1cafaa086ec24e5d3538b08aef" {
@@ -153,7 +182,7 @@ resource "cloudflare_record" "terraform_managed_resource_c6c12f1cafaa086ec24e5d3
   ttl     = 1
   type    = "CNAME"
   value   = "subspace.network"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_f62094c6234d9f68bcd933a0f47d3d64" {
@@ -162,7 +191,7 @@ resource "cloudflare_record" "terraform_managed_resource_f62094c6234d9f68bcd933a
   ttl     = 1
   type    = "CNAME"
   value   = "dns1.p08.nsone.net"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_0082afc8518c5c691215b445f53c6654" {
@@ -171,7 +200,7 @@ resource "cloudflare_record" "terraform_managed_resource_0082afc8518c5c691215b44
   ttl     = 3600
   type    = "CNAME"
   value   = "websites.weglot.com"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_83bbe72d68962e5c6102f1f6118cb626" {
@@ -180,7 +209,7 @@ resource "cloudflare_record" "terraform_managed_resource_83bbe72d68962e5c6102f1f
   ttl     = 3600
   type    = "CNAME"
   value   = "s1.domainkey.u27463899.wl150.sendgrid.net"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_e7d319fe6cdb95b5ead29b78f9f25063" {
@@ -189,7 +218,7 @@ resource "cloudflare_record" "terraform_managed_resource_e7d319fe6cdb95b5ead29b7
   ttl     = 3600
   type    = "CNAME"
   value   = "s2.domainkey.u27463899.wl150.sendgrid.net"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_409a596793c8d298ec5ebdfecbb111e7" {
@@ -198,7 +227,7 @@ resource "cloudflare_record" "terraform_managed_resource_409a596793c8d298ec5ebdf
   ttl     = 3600
   type    = "CNAME"
   value   = "proxy-ssl.webflow.com"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_31b86be89143160021c6bd9afdaf83b3" {
@@ -207,7 +236,7 @@ resource "cloudflare_record" "terraform_managed_resource_31b86be89143160021c6bd9
   ttl     = 1
   type    = "CNAME"
   value   = "subspace.network"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_b80fc952b5d2edbf7843fdfc91762d56" {
@@ -216,7 +245,7 @@ resource "cloudflare_record" "terraform_managed_resource_b80fc952b5d2edbf7843fdf
   ttl     = 1
   type    = "CNAME"
   value   = "subspace.network"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_88801c0aa1969b1b05af21ae13b0f922" {
@@ -225,7 +254,7 @@ resource "cloudflare_record" "terraform_managed_resource_88801c0aa1969b1b05af21a
   ttl     = 3600
   type    = "CNAME"
   value   = "aries-test-rpc-a.subspace.network"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_1b1accbe56b4fc84e9f57ba3737ec88c" {
@@ -234,7 +263,7 @@ resource "cloudflare_record" "terraform_managed_resource_1b1accbe56b4fc84e9f57ba
   ttl     = 3600
   type    = "CNAME"
   value   = "websites.weglot.com"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_3c4892d732cd9598d969e23b98fe8e29" {
@@ -243,7 +272,7 @@ resource "cloudflare_record" "terraform_managed_resource_3c4892d732cd9598d969e23
   ttl     = 3600
   type    = "CNAME"
   value   = "websites.weglot.com"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_113772a997574a65548468b238009d49" {
@@ -252,7 +281,7 @@ resource "cloudflare_record" "terraform_managed_resource_113772a997574a65548468b
   ttl     = 3600
   type    = "CNAME"
   value   = "websites.weglot.com"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_a50726853ab98cd0a23b0d67f69a62ba" {
@@ -261,7 +290,7 @@ resource "cloudflare_record" "terraform_managed_resource_a50726853ab98cd0a23b0d6
   ttl     = 3600
   type    = "CNAME"
   value   = "proxy-ssl.webflow.com"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_f28701c7b10678abc190c13b5906dbe1" {
@@ -270,7 +299,7 @@ resource "cloudflare_record" "terraform_managed_resource_f28701c7b10678abc190c13
   ttl     = 3600
   type    = "CNAME"
   value   = "websites.weglot.com"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_d5b9ae2a99064d91811927a96fda5d57" {
@@ -279,7 +308,7 @@ resource "cloudflare_record" "terraform_managed_resource_d5b9ae2a99064d91811927a
   ttl     = 3600
   type    = "TXT"
   value   = "v=TLSRPTv1; rua=mailto:g5oqy2di@tls.us.dmarcian.com"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_6aa9914d5ed8da851e945e032178034c" {
@@ -288,7 +317,7 @@ resource "cloudflare_record" "terraform_managed_resource_6aa9914d5ed8da851e945e0
   ttl     = 3600
   type    = "TXT"
   value   = "google-site-verification=f2RfDFH2l59GizXR0tKkIZ2i-M1LZjFFb-KG1dW61VM"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_602bacb1de51c879deb6224bf816d853" {
@@ -297,7 +326,7 @@ resource "cloudflare_record" "terraform_managed_resource_602bacb1de51c879deb6224
   ttl     = 3600
   type    = "TXT"
   value   = "google-site-verification=rSM4tCtwIT6_GKtHdpeRqUFSY3UQRzPuMe7qXc6OH8A"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_61eda5c4af93ca9d0f50e2fe1b56ae6d" {
@@ -306,11 +335,5 @@ resource "cloudflare_record" "terraform_managed_resource_61eda5c4af93ca9d0f50e2f
   ttl     = 3600
   type    = "TXT"
   value   = "google-site-verification=Fxi1PlaR_c4E2l34q5G5QoiMPc7PeRqY_sLpD4XiAxg"
-  zone_id = data.cloudflare_zone.cloudflare_zone.id
-}
-
-
-// ## Subspace.net zone ##
-data "cloudflare_zone" "cloudflare_zone_subspace_net" {
-  name = "subspace.net"
+  zone_id = data.cloudflare_zone.subspace_network.id
 }
