5365738aed890e6cf164922b0a4c0d8f7fcf59eb
DaMandal0rian <mando.github@proton.me>
2023-10-24 19:27:09 +0300

    add traefik proxy and replace caddy

--- aws/network-primitives/scripts/prune_docker_system.sh
+++ aws/network-primitives/scripts/prune_docker_system.sh
@@ -4,6 +4,6 @@ sudo docker ps -aq | xargs sudo docker update --restart=no
 sudo docker compose down
 # prune everything docker related
 sudo docker system prune -a -f
-sudo docker volume ls -q | grep -v caddy | xargs sudo docker volume rm -f
+sudo docker volume ls -q | grep -v traefik | xargs sudo docker volume rm -f
 # remove key files
 sudo rm -rf ~/subspace/*_keys.txt
