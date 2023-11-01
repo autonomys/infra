a88db6adea88ccf075dc5f3c3b9f2a76b1487641
DaMandal0rian <3614052+DaMandal0rian@users.noreply.github.com>
2023-11-01 16:42:40 +0300

    changes for astral (#198)
    - add monitoring
    - change endpoints to nova from evm
    - add variables
    - fix postgres config mounting for squid

--- explorer/terraform/aws/ec2/gemini-3g/modules/squids/main.tf
+++ explorer/terraform/aws/ec2/gemini-3g/modules/squids/main.tf
@@ -1,31 +1,31 @@
 module "squids" {
-  source           = "../../base/"
-  path_to_scripts  = "../../base/scripts"
-  path_to_configs  = "../../base/config"
-  network_name     = var.network_name
+  source          = "../../base/"
+  path_to_scripts = "../../base/scripts"
+  path_to_configs = "../../base/config"
+  network_name    = var.network_name
 
   blue-squid-node-config = {
-    deployment-color     = "blue"
-    network-name         = "${var.network_name}-blue"
-    domain-prefix        = "evm"
-    instance-type        = var.instance_type
-    deployment-version   = 0
-    regions              = var.aws_region
-    instance-count-blue  = var.instance_count_blue
-    disk-volume-size     = var.disk_volume_size
-    disk-volume-type     = var.disk_volume_type
-    prune                = false
-    environment          = "staging"
+    deployment-color    = "blue"
+    network-name        = "${var.network_name}-blue"
+    domain-prefix       = "nova"
+    instance-type       = var.instance_type
+    deployment-version  = 0
+    regions             = var.aws_region
+    instance-count-blue = var.instance_count_blue
+    disk-volume-size    = var.disk_volume_size
+    disk-volume-type    = var.disk_volume_type
+    prune               = false
+    environment         = "staging"
   }
 
   green-squid-node-config = {
     deployment-color     = "green"
     network-name         = "${var.network_name}-green"
-    domain-prefix        = "evm"
+    domain-prefix        = "nova"
     instance-type        = var.instance_type
     deployment-version   = 0
     regions              = var.aws_region
-    instance-count-green  = var.instance_count_green
+    instance-count-green = var.instance_count_green
     disk-volume-size     = var.disk_volume_size
     disk-volume-type     = var.disk_volume_type
     prune                = false
@@ -33,21 +33,23 @@ module "squids" {
   }
 
   archive-node-config = {
-    network-name         = "${var.network_name}"
-    domain-prefix        = "evm"
-    instance-type        = var.instance_type
-    deployment-version   = 0
-    regions              = var.aws_region
-    instance-count       = 1
-    disk-volume-size     = var.disk_volume_size
-    disk-volume-type     = var.disk_volume_type
-    prune                = false
+    network-name       = "${var.network_name}"
+    domain-prefix      = "nova"
+    instance-type      = var.instance_type
+    deployment-version = 0
+    regions            = var.aws_region
+    instance-count     = 1
+    disk-volume-size   = var.disk_volume_size
+    disk-volume-type   = var.disk_volume_type
+    prune              = false
   }
 
   cloudflare_api_token = var.cloudflare_api_token
   cloudflare_email     = var.cloudflare_email
   aws_key_name         = var.aws_key_name
-  datadog_api_key      = var.datadog_api_key
+  nr_api_key           = var.nr_api_key
+  netdata_token        = var.netdata_token
+  netdata_room         = var.netdata_room
   access_key           = var.access_key
   secret_key           = var.secret_key
   vpc_id               = var.vpc_id
