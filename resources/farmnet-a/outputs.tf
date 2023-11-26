5ccecc7c35c0f8dc19079f138f0945c9fe7a7b97
DaMandal0rian <mando.github@proton.me>
2023-11-26 18:53:43 +0300

    cleanup infra repo
    remove old digital ocean resources
    remove old network primitives

--- resources/farmnet-a/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ip_address_aries-farmnet-a_droplet" {
-  value       = digitalocean_droplet.aries-farmnet-a.ipv4_address
-  description = "ip_address_droplet: "
-}
--- resources/telemetry/projects.tf
+++
@@ -1,6 +0,0 @@
-resource "digitalocean_project" "telemetry-subspace" {
-  name      = "Telemetry Subspace"
-  purpose   = "Service or API"
-  resources = [digitalocean_droplet.telemetry-subspace-frontend-backend.urn]
-}
-
