a6e787315aa81b11d80b35b66846d3da0b67301c
DaMandal0rian <3614052+DaMandal0rian@users.noreply.github.com>
2023-11-30 17:24:11 +0300

    cleanup infra repo (#221)
    remove old digital ocean resources
    remove old network primitives

--- resources/telemetry/droplets.tf
+++
@@ -1,9 +0,0 @@
-resource "digitalocean_droplet" "telemetry-subspace-frontend-backend" {
-  image  = "ubuntu-20-04-x64"
-  name   = "telemetry-subspace-frontend-backend"
-  region = "nyc3"
-  size   = "s-8vcpu-16gb"
-  ssh_keys = [
-    data.digitalocean_ssh_key.nazar-key.id,
-  ]
-}
--- resources/telemetry/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ip_address_droplet" {
-  value       = digitalocean_droplet.telemetry-subspace-frontend-backend.ipv4_address
-  description = "ip_address_droplet: "
-}
