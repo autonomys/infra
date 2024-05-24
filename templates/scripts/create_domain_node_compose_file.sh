0550d9ef919c257698ed03bd80e4939285fcbccc
DaMandal0rian <mando.github@proton.me>
2024-05-24 19:42:43 +0300

    bump traefik release

--- templates/scripts/create_domain_node_compose_file.sh
+++ templates/scripts/create_domain_node_compose_file.sh
@@ -47,7 +47,7 @@ services:
 
   # traefik reverse proxy with automatic tls management using let encrypt
   traefik:
-    image: traefik:v2.10
+    image: traefik:v2.11.3
     container_name: traefik
     restart: unless-stopped
     command:
