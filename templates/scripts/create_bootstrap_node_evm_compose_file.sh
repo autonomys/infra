db92e6b0018ca040814c026b5991146f28bdff06
DaMandal0rian <mando.github@proton.me>
2024-07-26 18:28:44 +0300

    add full sync parameter
    temp fix for https://github.com/autonomys/subspace/issues/2950

--- templates/scripts/create_bootstrap_node_evm_compose_file.sh
+++ templates/scripts/create_bootstrap_node_evm_compose_file.sh
@@ -136,6 +136,7 @@ cat >> ~/subspace/docker-compose.yml << EOF
       "--base-path", "/var/subspace",
       "--state-pruning", "archive",
       "--blocks-pruning", "256",
+      "--sync", "full",
       "--pot-external-entropy", "\${POT_EXTERNAL_ENTROPY}",
       "--listen-on", "/ip4/0.0.0.0/tcp/30333",
       "--listen-on", "/ip6/::/tcp/30333",
