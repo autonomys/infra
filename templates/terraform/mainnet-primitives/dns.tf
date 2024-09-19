fcfa0ada01b3980adc46da2fd0bbed603aed0337
DaMandal0rian <mando.github@proton.me>
2024-09-19 11:09:15 +0300

    add root module for mainnet network primitives

---
+++ templates/terraform/mainnet-primitives/dns.tf
@@ -0,0 +1,106 @@
+data "cloudflare_zone" "cloudflare_zone" {
+  name = "subspace.network"
+}
+
+resource "cloudflare_record" "rpc" {
+  count   = length(local.rpc_nodes_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "${var.rpc-node-config.domain-prefix}-${count.index}.${var.network_name}"
+  value   = local.rpc_nodes_ip_v4[count.index]
+  type    = "A"
+}
+
+resource "cloudflare_record" "nova" {
+  count   = length(local.evm_nodes_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "${var.evm-node-config.domain-prefix[0]}-${var.evm-node-config.domain-id[0]}.${var.network_name}"
+  value   = local.evm_nodes_ip_v4[count.index]
+  type    = "A"
+}
+
+resource "cloudflare_record" "nova_ipv6" {
+  count   = length(local.evm_nodes_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "${var.evm-node-config.domain-prefix[0]}-${var.evm-node-config.domain-id[0]}.${var.network_name}"
+  value   = local.evm_nodes_ip_v6[count.index]
+  type    = "AAAA"
+
+resource "cloudflare_record" "rpc-indexer" {
+  count   = length(local.rpc_indexer_nodes_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "${var.rpc-indexer-node-config.domain-prefix}-${count.index}.${var.network_name}"
+  value   = local.rpc_indexer_nodes_ip_v4[count.index]
+  type    = "A"
+}
+
+resource "cloudflare_record" "nova-indexer-rpc" {
+  count   = length(local.nova_indexer_nodes_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "${var.nova-indexer-node-config.domain-prefix}-${count.index}.${var.network_name}"
+  value   = local.nova_indexer_nodes_ip_v4[count.index]
+  type    = "A"
+}
+
+resource "cloudflare_record" "auto" {
+  count   = length(local.evm_nodes_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "${var.autoid-node-config.domain-prefix[1]}-${var.autoid-node-config.domain-id[1]}.${var.network_name}"
+  value   = local.evm_nodes_ip_v4[count.index]
+  type    = "A"
+}
+
+resource "cloudflare_record" "auto_ipv6" {
+  count   = length(local.evm_nodes_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "${var.autoid-node-config.domain-prefix[1]}-${var.autoid-node-config.domain-id[1]}.${var.network_name}"
+  value   = local.evm_nodes_ip_v6[count.index]
+  type    = "AAAA"
+}
+
+resource "cloudflare_record" "bootstrap" {
+  count   = length(local.bootstrap_nodes_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "bootstrap-${count.index}.${var.network_name}"
+  value   = local.bootstrap_nodes_ip_v4[count.index]
+  type    = "A"
+}
+
+resource "cloudflare_record" "bootstrap_ipv6" {
+  count   = length(local.bootstrap_nodes_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "bootstrap-${count.index}.${var.network_name}"
+  value   = local.bootstrap_nodes_ip_v6[count.index]
+  type    = "AAAA"
+}
+
+resource "cloudflare_record" "bootstrap_evm" {
+  count   = length(local.bootstrap_nodes_evm_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "bootstrap-${count.index}.nova.${var.network_name}"
+  value   = local.bootstrap_nodes_evm_ip_v6[count.index]
+  type    = "A"
+}
+
+resource "cloudflare_record" "bootstrap_evm_ipv6" {
+  count   = length(local.bootstrap_nodes_evm_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "bootstrap-${count.index}.nova.${var.network_name}"
+  value   = local.bootstrap_nodes_evm_ip_v6[count.index]
+  type    = "AAAA"
+}
+
+resource "cloudflare_record" "bootstrap_auto" {
+  count   = length(local.bootstrap_nodes_autoid_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "bootstrap-${count.index}.auto.${var.network_name}"
+  value   = local.bootstrap_nodes_autoid_ip_v6[count.index]
+  type    = "A"
+}
+
+resource "cloudflare_record" "bootstrap_auto_ipv6" {
+  count   = length(local.bootstrap_nodes_autoid_ip_v4)
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
+  name    = "bootstrap-${count.index}.auto.${var.network_name}"
+  value   = local.bootstrap_nodes_autoid_ip_v6[count.index]
+  type    = "AAAA"
+}
---
+++ templates/terraform/mainnet-primitives/outputs.tf
@@ -0,0 +1,159 @@
+// Output Variables
+
+output "bootstrap_node_server_id" {
+  value = aws_instance.bootstrap_node.*.id
+}
+
+output "bootstrap_node_public_ip" {
+  value = aws_instance.bootstrap_node.*.public_ip
+}
+
+output "bootstrap_node_private_ip" {
+  value = aws_instance.bootstrap_node.*.private_ip
+}
+
+output "bootstrap_node_ami" {
+  value = aws_instance.bootstrap_node.*.ami
+}
+
+output "bootstrap_node_evm_server_id" {
+  value = aws_instance.bootstrap_node_evm.*.id
+}
+
+output "bootstrap_node_evm_public_ip" {
+  value = aws_instance.bootstrap_node_evm.*.public_ip
+}
+
+output "bootstrap_node_evm_private_ip" {
+  value = aws_instance.bootstrap_node_evm.*.private_ip
+}
+
+output "bootstrap_node_evm_ami" {
+  value = aws_instance.bootstrap_node_evm.*.ami
+}
+
+output "bootstrap_node_autoid_server_id" {
+  value = aws_instance.bootstrap_node_autoid.*.id
+}
+
+output "bootstrap_node_autoid_public_ip" {
+  value = aws_instance.bootstrap_node_autoid.*.public_ip
+}
+
+output "bootstrap_node_autoid_private_ip" {
+  value = aws_instance.bootstrap_node_autoid.*.private_ip
+}
+
+output "bootstrap_node_autoid_ami" {
+  value = aws_instance.bootstrap_node_autoid.*.ami
+}
+
+output "rpc_indexer_node_server_id" {
+  value = aws_instance.rpc_indexer_node.*.id
+}
+
+output "rpc_indexer_node_private_ip" {
+  value = aws_instance.rpc_indexer_node.*.private_ip
+}
+
+output "rpc_indexer_node_public_ip" {
+  value = aws_instance.rpc_indexer_node.*.public_ip
+}
+
+output "rpc_indexer_node_ami" {
+  value = aws_instance.rpc_indexer_node.*.ami
+}
+
+output "nova_indexer_node_server_id" {
+  value = aws_instance.nova_indexer_node.*.id
+}
+
+output "nova_indexer_node_private_ip" {
+  value = aws_instance.nova_indexer_node.*.private_ip
+}
+
+output "nova_indexer_node_public_ip" {
+  value = aws_instance.nova_indexer_node.*.public_ip
+}
+
+output "nova_indexer_node_ami" {
+  value = aws_instance.nova_indexer_node.*.ami
+}
+
+
+output "rpc_node_server_id" {
+  value = aws_instance.rpc_node.*.id
+}
+
+output "rpc_node_private_ip" {
+  value = aws_instance.rpc_node.*.private_ip
+}
+
+output "rpc_node_public_ip" {
+  value = aws_instance.rpc_node.*.public_ip
+}
+
+output "rpc_node_ami" {
+  value = aws_instance.rpc_node.*.ami
+}
+
+
+output "evm_node_server_id" {
+  value = aws_instance.evm_node.*.id
+}
+
+output "evm_node_private_ip" {
+  value = aws_instance.evm_node.*.private_ip
+}
+
+output "evm_node_public_ip" {
+  value = aws_instance.evm_node.*.public_ip
+}
+
+output "evm_node_ami" {
+  value = aws_instance.evm_node.*.ami
+}
+
+output "autoid_node_server_id" {
+  value = aws_instance.autoid_node.*.id
+}
+
+output "autoid_node_private_ip" {
+  value = aws_instance.autoid_node.*.private_ip
+}
+
+output "autoid_node_public_ip" {
+  value = aws_instance.autoid_node.*.public_ip
+}
+
+output "autoid_node_ami" {
+  value = aws_instance.autoid_node.*.ami
+}
+
+
+output "farmer_node_server_id" {
+  value = aws_instance.farmer_node.*.id
+}
+
+output "farmer_node_private_ip" {
+  value = aws_instance.farmer_node.*.private_ip
+}
+
+output "farmer_node_public_ip" {
+  value = aws_instance.farmer_node.*.public_ip
+}
+
+output "farmer_node_ami" {
+  value = aws_instance.farmer_node.*.ami
+}
+
+output "dns-records" {
+  value = [
+    cloudflare_record.bootstrap.*.hostname,
+    cloudflare_record.rpc.*.hostname,
+    cloudflare_record.rpc-indexer.*.hostname,
+    cloudflare_record.nova-indexer-rpc.*.hostname,
+    cloudflare_record.nova.*.hostname,
+    cloudflare_record.autoid.*.hostname,
+  ]
+}
