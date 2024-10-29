727d75ff69f48c90f6b9b1c1d371fa85e19ff0cd
DaMandal0rian <3614052+DaMandal0rian@users.noreply.github.com>
2024-10-29 14:56:09 +0300

    modify node key script to use AWS kms (#358)

--- scripts/generate_node_keys.sh
+++ scripts/generate_node_keys.sh
@@ -19,23 +19,23 @@ generate_node_key() {
 
   local key_file="${node_type}-${node_index}.key"
   subkey generate-node-key 2>&1 | tee "${key_file}"
-  echo "Generated node key for ${node_type}-${node_index}"
+  echo -e "\nGenerated node key for ${node_type}-${node_index}"
 }
 
-# Function to add key-value pair to Hashicorp Vault
-add_to_vault() {
+# Function to add key-value pair to AWS KMS
+add_to_kms() {
   local key_file=$1
 
   local key_id=$(sed -n '1p' "${key_file}")
   local key_value=$(sed -n '2p' "${key_file}")
   local node_name=$(basename "${key_file}" .key)
 
-  # Add key-value pair to Hashicorp Vault using API call
-  local vault_endpoint="https://vault.eks.subspace.network/v1/secret/data/node-keys/${node_name}"
-  local vault_token=$(base64 -d <<< ${VAULT_TOKEN})
+  # Add key-value pair to AWS KMS using API call
+  local kms_key_id="arn:aws:kms:us-west-2:123456789012:key/abcd1234-a123-1234-a123-123456789012" # Replace with your KMS key ID
+  local aws_region="us-east-2"
 
-  curl -X POST -H "X-Vault-Token: ${vault_token}" -d "{\"data\": {\"key\": \"${key_id}=${key_value}\"}}" "${vault_endpoint}"
-  echo "Added key-value pair to Hashicorp Vault: ${node_name} = ${key_id}=${key_value}"
+  aws kms encrypt --key-id ${kms_key_id} --plaintext "key=${key_id} value=${key_value}" --region ${aws_region}
+  echo -e "\nAdded key-value pair to AWS KMS: ${node_name} = ${key_id}=${key_value}"
 }
 
 # Check and install subkey if needed
@@ -51,9 +51,14 @@ for i in {0..1}; do
   generate_node_key "rpc" "${i}"
 done
 
-# Generate node keys for domain nodes
+# Generate node keys for nova nodes
 for i in {0..1}; do
-  generate_node_key "domain" "${i}"
+  generate_node_key "nova" "${i}"
+done
+
+# Generate node keys for autoid nodes
+for i in {0..1}; do
+  generate_node_key "autoid" "${i}"
 done
 
 # Generate node key for farmer node
@@ -64,9 +69,14 @@ for i in {0..1}; do
   generate_node_key "nova-bootstrap" "${i}"
 done
 
-# Add generated keys to Hashicorp Vault
+# Generate node keys for autoid-bootstrap nodes
+for i in {0..1}; do
+  generate_node_key "autoid-bootstrap" "${i}"
+done
+
+# Add generated keys to AWS KMS
 for key_file in *.key; do
-  add_to_vault "${key_file}"
+  add_to_kms "${key_file}"
 done
 
 echo "Node key generation and Vault storage completed."
