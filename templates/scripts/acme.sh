eb92d28e767f525412894d05b74f41521e8bf666
DaMandal0rian <mando.github@proton.me>
2024-11-27 19:17:09 +0300

    fix acme.sh script

--- templates/scripts/acme.sh
+++ templates/scripts/acme.sh
@@ -3,12 +3,21 @@
 set -e
 
 DIRECTORY="${HOME}/subspace"
+ACME_FILE="$DIRECTORY/acme.json"
+
+# Create directory if it doesn't exist
 if [ ! -d "$DIRECTORY" ]; then
-    mkdir "$DIRECTORY"
+    mkdir -p "$DIRECTORY"
     echo "Directory '$DIRECTORY' created."
-    touch $DIRECTORY/acme.json
-    chmod 600 $DIRECTORY/acme.json
-    echo "ACME '$DIRECTORY/acme.json' file created."
 else
     echo "Directory '$DIRECTORY' already exists."
+fi
+
+# Create ACME file if it doesn't exist
+if [ ! -f "$ACME_FILE" ]; then
+    touch "$ACME_FILE"
+    chmod 600 "$ACME_FILE"
+    echo "ACME file '$ACME_FILE' created and permissions set."
+else
+    echo "ACME file '$ACME_FILE' already exists."
 fi
