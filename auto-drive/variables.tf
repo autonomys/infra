89e8170793b2caa8a68fff31afd95973e082b37e
DaMandal0rian <3614052+DaMandal0rian@users.noreply.github.com>
2025-02-13 16:03:29 +0300

    add another public auto-drive instance (#413)

--- auto-drive/variables.tf
+++ auto-drive/variables.tf
@@ -70,7 +70,7 @@ variable "kms_key_id" {
 variable "auto_drive_instance_count" {
   description = "Number of auto-drive instances to create."
   type        = number
-  default     = 1
+  default     = 2
 }
 
 variable "gateway_instance_count" {
