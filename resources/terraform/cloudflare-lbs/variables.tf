e354fb753b6d8e129c02141507b3f5f3e25e7eda
DaMandal0rian <mando.github@proton.me>
2025-07-23 23:32:40 +0300

    import cloudflare load-balancers to terraform

---
+++ resources/terraform/cloudflare-lbs/variables.tf
@@ -0,0 +1,16 @@
+# Variables
+variable "cloudflare_api_token" {
+  description = "Cloudflare API Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "cloudflare_account_id" {
+  description = "Cloudflare Account ID"
+  type        = string
+}
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare Zone ID"
+  type        = string
+}
