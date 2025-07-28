db29d5638a97bd82d5ca55899799a9da80830c3d
DaMandal0rian <3614052+DaMandal0rian@users.noreply.github.com>
2025-07-28 04:13:54 +0300

    Gh dedicated runners amis (#453)
    * update ami search
    
    * remove old GH dedicated runner module

--- resources/terraform/github-runners/terraform/dedicated/scripts/cleanup_script_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Running cleanup script for Linux..."
-rm -rf $HOME/actions-runner/_work/*
-rm -rf $HOME/actions-runner/_diag/*
-echo "Cleanup script completed."
--- resources/terraform/github-runners/terraform/dedicated/scripts/cleanup_script_macos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-echo "Running cleanup script macOS..."
-rm -rf /Users/ec2-user/actions-runner/_work/
-rm -rf /Users/ec2-user/actions-runner/_diag/
-rm -rf /Users/ec2-user/Library/Keychains/build.keychain-db
-rm -rf /Users/ec2-user/bin/gon
-echo "Cleanup script completed."
