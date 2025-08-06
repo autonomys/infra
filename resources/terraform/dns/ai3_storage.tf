e1d39ea17f4053226cc704455f3814a3e53244af
Vedhavyas Singareddi <7549475+vedhavyas@users.noreply.github.com>
2025-08-06 12:16:02 +0530

    remove outdated dns and import dns for ai3.storage

---
+++ resources/terraform/dns/ai3_storage.tf
@@ -0,0 +1,20 @@
+resource "cloudflare_dns_record" "ai3_storage_apex_loadbalancer" {
+  content  = "apex-loadbalancer.netlify.com"
+  name     = "ai3.storage"
+  proxied  = false
+  ttl      = 1
+  type     = "CNAME"
+  zone_id  = data.cloudflare_zone.ai3_storage.zone_id
+  settings = {}
+}
+
+resource "cloudflare_dns_record" "ai3_storage_auto_drive_demo" {
+  content  = "auto-drive-demo.netlify.app"
+  name     = "www.ai3.storage"
+  proxied  = false
+  ttl      = 1
+  type     = "CNAME"
+  zone_id  = data.cloudflare_zone.ai3_storage.zone_id
+  settings = {}
+}
+
