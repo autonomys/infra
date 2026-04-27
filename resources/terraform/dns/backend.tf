0456c7c07ca08889e509be5287614e05be4adf6f
Vedhavyas Singareddi <7549475+vedhavyas@users.noreply.github.com>
2026-04-27 10:21:46 +0530

    Add autonomys.org zone with Google Workspace MX record and a small cleanup

--- resources/terraform/dns/backend.tf
+++ resources/terraform/dns/backend.tf
@@ -6,4 +6,15 @@ terraform {
       name = "dns"
     }
   }
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "5.8.2"
+    }
+  }
+}
+
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
