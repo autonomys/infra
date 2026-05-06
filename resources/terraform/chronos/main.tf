456729b8ae6d32d813d7354e0eb8b8b271f8167a
Vedhavyas Singareddi <7549475+vedhavyas@users.noreply.github.com>
2026-05-06 14:54:30 +0530

    Bump chronos domain-rpc-node to chronos-2026-apr-30

--- resources/terraform/chronos/main.tf
+++ resources/terraform/chronos/main.tf
@@ -94,7 +94,7 @@ module "chronos" {
       {
         domain-id     = 0
         domain-name   = "auto-evm"
-        docker-tag    = "mainnet-2026-feb-28"
+        docker-tag    = "chronos-2026-apr-30"
         reserved-only = false
         index         = 0
         sync-mode     = "full"
