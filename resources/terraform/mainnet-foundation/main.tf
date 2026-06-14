2ce8d287346e382b5937c7e7a42b09daf1db06c3
Vedhavyas Singareddi <7549475+vedhavyas@users.noreply.github.com>
2026-06-14 21:57:07 +0530

    Bump mainnet foundation consensus bootstrap to mainnet-2026-jun-14

--- resources/terraform/mainnet-foundation/main.tf
+++ resources/terraform/mainnet-foundation/main.tf
@@ -14,7 +14,7 @@ module "mainnet_foundation" {
     genesis-hash = "66455a580aabff303720aa83adbe6c44502922251c03ba73686d5245da9e21bd"
     bootstrap-nodes = [
       {
-        docker-tag    = "mainnet-2026-jun-08"
+        docker-tag    = "mainnet-2026-jun-14"
         reserved-only = false
         index         = 0
         sync-mode     = "full"
