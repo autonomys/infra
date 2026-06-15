8d822aca7e45330f7665614f15f4a5c4060bfd47
Vedhavyas Singareddi <7549475+vedhavyas@users.noreply.github.com>
2026-06-15 09:41:47 +0530

    Bump chronos timekeeper-1 to chronos-2026-jun-14

--- resources/terraform/chronos/main.tf
+++ resources/terraform/chronos/main.tf
@@ -57,7 +57,7 @@ module "chronos" {
         ipv4          = var.timekeeper_node_ipv4.timekeeper-0
       },
       {
-        docker-tag    = "chronos-2026-jun-01"
+        docker-tag    = "chronos-2026-jun-14"
         reserved-only = false
         index         = 1
         sync-mode     = "full"
