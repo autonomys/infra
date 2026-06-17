4d706f6e6b05641fcbfdab048e8c4d0497c03a8d
Vedhavyas Singareddi <7549475+vedhavyas@users.noreply.github.com>
2026-06-17 16:09:50 +0530

    redeploy uptime-kuma on box replacement + ensure compose dir exists

--- resources/terraform/telemetry/deploy.tf
+++ resources/terraform/telemetry/deploy.tf
@@ -1,9 +1,10 @@
 resource "null_resource" "deploy_uptime_kuma" {
   depends_on = [aws_instance.telemetry]
 
-  # redeploy when the compose changes
+  # redeploy when the compose changes or the box is replaced
   triggers = {
     compose_sha = filesha256("${path.module}/uptime-kuma/docker-compose.yml")
+    instance_id = aws_instance.telemetry.id
   }
 
   connection {
@@ -14,6 +15,12 @@ resource "null_resource" "deploy_uptime_kuma" {
     timeout = "120s"
   }
 
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /home/${var.ssh_user}/uptime-kuma/docker",
+    ]
+  }
+
   provisioner "file" {
     source      = "${path.module}/uptime-kuma/docker-compose.yml"
     destination = "/home/${var.ssh_user}/uptime-kuma/docker/docker-compose.yml"
@@ -21,7 +28,7 @@ resource "null_resource" "deploy_uptime_kuma" {
 
   provisioner "remote-exec" {
     inline = [
-      "cd /home/${var.ssh_user}/uptime-kuma/docker && sudo docker compose up -d"
+      "cd /home/${var.ssh_user}/uptime-kuma/docker && sudo docker compose up -d",
     ]
   }
 }
